5add0662af711b40d5f9428f828203fdac9c777a
ABDURAZZ <abdurazzokbossov@gmail.com>
2024-08-27 10:17:36 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,25 @@
+let havta = prompt('son kiritin 1-7gacha')
+switch (havta) {
+    case '1':
+        alert('dushanba')
+        break;
+    case '2':
+        alert('seshanba')
+        break;
+    case '3':
+        alert('chorshanba')
+        break;
+    case '4':
+        alert('payshanba')
+        break;
+    case '5':
+        alert('juma')
+        break;
+        case '6':
+        alert('sanba')
+        case '7':
+         alert('yakshanba')
+         break;
+    default:
+}
+
